e6daa133a70832d7bf9ad0d0b02c96e232835821
JuanZzz4 <149608503+JuanZzz4@users.noreply.github.com>
2023-12-13 00:21:27 -0500

    Update EnviarMensajes.py

--- EnviarMensajes.py
+++ EnviarMensajes.py
@@ -2,12 +2,55 @@ import psutil
 import json
 import paho.mqtt.client as mqtt
 import time
-import uuid
 from datetime import datetime
+import mysql.connector
 
+# Configuración de MQTT
 mqtt_broker = "broker.hivemq.com"
 mqtt_port = 1883
 mqtt_topic = "Taller arquitectura"
+
+# Configuración de MySQL
+mysql_host = "localhost"
+mysql_user = "root"
+mysql_password = "mi_contraseña"
+mysql_database = "mi_base_de_datos"
+
+def insertar_en_mysql(datos):
+    connection = None  # Inicializar la variable connection
+    try:
+        connection = mysql.connector.connect(
+            host=mysql_host,
+            user=mysql_user,
+            password=mysql_password,
+            database=mysql_database
+        )
+
+        cursor = connection.cursor()
+
+        consulta = """
+            INSERT INTO mediciones (cpu, memory, bytes_enviados, bytes_recibidos, mac_address, temperature, fecha_hora)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+
+        cursor.execute(consulta, (
+            datos["cpu"],
+            datos["memory"],
+            datos["net"]["bytes_enviados"],
+            datos["net"]["bytes_recibidos"],
+            datos["mac_address"],
+            datos["temperature"],
+            datos["date"]
+        ))
+
+        connection.commit()
+        print("Datos insertados en MySQL:", datos)
+    except Exception as e:
+        print("Error al insertar en MySQL:", str(e))
+    finally:
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
 
 def enviar_metricas(client):
     cpu_percent = psutil.cpu_percent()
@@ -29,8 +72,12 @@ def enviar_metricas(client):
         "date": fecha_hora_actual
     }
 
+    # Envío de datos a MQTT
     client.publish(mqtt_topic, json.dumps(datos_json))
     print("Datos enviados:", datos_json)
+
+    # Inserción de datos en MySQL
+    insertar_en_mysql(datos_json)
 
 def obtener_temperatura():
     try:
@@ -69,4 +116,3 @@ finally:
     client.loop_stop()
     client.disconnect()
     print("Conexión cerrada.")
-
